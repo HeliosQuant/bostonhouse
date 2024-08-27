5a422ebdad5ddd29ce3d7dce531bc032f0b9a85b
Kenja Koa <othmane.fathallah.1@outlook.com>
2024-08-27 01:42:06 +0100

    Web app is ready

--- app.py
+++ app.py
@@ -24,6 +24,15 @@ def predict_api():
     output = regmodel.predict(new_data)
     print(output)
     return jsonify(output[0])
+@app.route("/predict", methods=["POST"])
+def predict():
+    form_data = request.form
+    data = [float(value) for ken,value in form_data.items()]
+    print(data)
+    final_input = scalar.transform(np.array(data).reshape(1, -1))
+    print(final_input)
+    output = regmodel.predict(final_input)[0]
+    return render_template("home.html", prediction_text="The House Price prediction is {}".format(output))
 
 if __name__ == "__main__":
     app.run(debug=True)
